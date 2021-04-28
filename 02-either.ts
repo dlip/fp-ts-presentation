c2f3972ff82a3eae9f6558dce227fa6ca3d2cb80
Dane Lipscombe <dane@lipcombe.com.au>
2021-04-28 11:30:08 +1000

    Init repo

---
+++ 02-either.ts
@@ -0,0 +1,52 @@
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
+
+type NetworkError = {
+    type: "NetworkError",
+    code: number,
+}
+
+type UserError = {
+    type: "UserError",
+    description: string,
+}
+
+type AppError = NetworkError | UserError;
+
+function getBalance(): E.Either<AppError, number> {
+    const ok = true;
+    return ok ? E.right(40)
+        : E.left({ type: "NetworkError", code: 404 });
+}
+
+function transfer(balance: number): E.Either<AppError, string> {
+    return balance > 50 ?
+        E.right("transfer complete") :
+        E.left({ type: "UserError", description: "Insufficient funds" });
+}
+
+function logic(): E.Either<AppError, string> {
+    return pipe(
+        getBalance(),
+        E.chain(transfer),
+    );
+}
+
+function handleError(e: AppError): string {
+    switch (e.type) {
+        case "NetworkError":
+            return `NetworkError ${e.code}`;
+        case "UserError":
+            return `UserError ${e.description}`;
+        default:
+            return "";
+    }
+}
+
+pipe(
+    logic(),
+    E.fold(
+        (e) => pipe(handleError(e), console.error),
+        console.log
+    )
+)
